844137566c89b46168716aed090cb52f6e70c673
kyehyon.Lee <khlee34@naver.com>
2021-12-10 22:42:25 +0900

    Update code.java

--- code.java
+++ code.java
@@ -6,4 +6,5 @@ public class A {
 	private final static String VERSION = "123";
 	private final static String CODE = "456";
 	private final static Integer A = 1000;
+	private final static Integer B = 20;
 }
