f77ba3583de56551e429046d81b2b359f196ced5
rey <rey@f-lab.kr>
2022-04-11 22:39:42 +0900

    충돌

--- code.java
+++ code.java
@@ -9,4 +9,6 @@ public class A {
         // feature/test 브랜치 생성 후 작업
         // 이건 작업 완료 후 develop로 PR 해야 됨
         private final static Integer V = 10;
+        // 이건 의도적인 충돌을 만든거 입니다.
+        private final static String CRASH = "crash1";
 }
