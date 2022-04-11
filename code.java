1ea59d6dcc2173862b96d108e68a3b7fce10fd41
rey <rey@f-lab.kr>
2022-04-11 22:33:08 +0900

    test 개발

--- code.java
+++ code.java
@@ -6,4 +6,7 @@ public class A {
 	private final static String VERSION = "123";
 	private final static String CODE = "456";
 	private final static Integer B = 40;
+        // feature/test 브랜치 생성 후 작업
+        // 이건 작업 완료 후 develop로 PR 해야 됨
+        private final static Integer V = 10;
 }
